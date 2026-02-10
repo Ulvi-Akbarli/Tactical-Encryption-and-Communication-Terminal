8b14c30843ddd77c9a40eb7389384a3799ec996f
Ulvi-Akbarli <ulvi.akber@mail.ru>
2026-02-10 18:59:25 +0400

    Add files via upload

---
+++ C#(WIndows-Form)_Taktiki Şifrələmə və Rabitə Terminalı/Taktiki Şifrələmə və Rabitə Terminalı/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Taktiki_Şifrələmə_və_Rabitə_Terminalı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(206, 158);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(360, 30);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.Lime;
+            this.btnSend.FlatAppearance.BorderSize = 0;
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.ForeColor = System.Drawing.Color.Black;
+            this.btnSend.Location = new System.Drawing.Point(206, 194);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(360, 41);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "GÖNDƏR";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Silver;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            " "});
+            this.listBox1.Location = new System.Drawing.Point(206, 299);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(360, 244);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(68, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(671, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "MİLLİ MÜDAFİƏ - KOMANDA MƏRKƏZİ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(201, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Əmri daxil edin :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(201, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(365, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Göndərilən mesajların tarixçəsi (Log)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(814, 555);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mərkəzi İdarəetmə Paneli (MİP)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ C#(WIndows-Form)_Taktiki Şifrələmə və Rabitə Terminalı/Taktiki Şifrələmə və Rabitə Terminalı/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+
+namespace Taktiki_Şifrələmə_və_Rabitə_Terminalı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new MqttClient("broker.hivemq.com");
+            client.Connect(Guid.NewGuid().ToString());
+        }
+
+        MqttClient client;
+        string key = "AZ-DEF-2025"; // Şifrə açarı
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string originalMessage = txtMessage.Text;
+
+            // XOR ilə Şifrələmə
+            byte[] encryptedBytes = EncryptDecrypt(originalMessage, key);
+
+            if (client.IsConnected)
+            {
+                client.Publish("milli_mudafie/terminal_001", encryptedBytes);
+                string status = $"[{DateTime.Now:HH:mm:ss}] Göndərildi: {originalMessage}";
+                listBox1.Items.Add(status);
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+            }else
+            {
+                MessageBox.Show("Bağlantı kəsildi! Yenidən qoşulun.");
+            }
+        }
+
+        byte[] EncryptDecrypt(string text, string key)
+        {
+            byte[] res = new byte[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                res[i] = (byte)(text[i] ^ key[i % key.Length]);
+            }
+            return res;
+        }
+    }
+}
